d7c8addfe5fde8d2dcc9783e21c1019579180652
Diana <mukharidiana@gmail.com>
2024-03-13 10:42:46 +0200

    startwith function added

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
       function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let result = '';
+    let numbers=/\d/;
+    let letters=/[a-zA-Z]/;
+   
+if(input.startsWith("pet_") ){
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+  result="valid syntax";
 }
+  else{
+    result="invalid syntax";
+  }
+
+  document.getElementById('result').innerText = result;
+
+
+}         
+      
