1602cd5f0226a60e77a006accd888471d8408615
Diana <mukharidiana@gmail.com>
2024-03-15 10:56:45 +0200

    special character function added

--- script.js
+++ script.js
@@ -4,15 +4,21 @@
     let result = '';
     let numbers=/\d/;
     let letters=/[a-zA-Z]/;
+    let SpecialChar= /[!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?]/;
    
 if(input.startsWith("pet_")   && letters.test(input.split('_')[1]) && numbers.test(input.split('_')[1]) ){
 
 
   result="valid syntax";
 }
+
   else{
     result="invalid syntax";
   }
+      if(SpecialChar.test(input))
+    {
+    result="invalid syntax";
+    }
 
   document.getElementById('result').innerText = result;
 
